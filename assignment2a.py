1205dfd4934d02e2cca93f566c7aa254ca660532
SebastianN8 <36240582+SebastianN8@users.noreply.github.com>
2018-02-26 13:25:55 -0500

    final version

--- assignment2a.py
+++ assignment2a.py
@@ -9,13 +9,20 @@
 #
 
 def calculate_area_of_a_circle(diameter_passed_in):
-	total = (3.14)*(diameter_passed_in/2)^2
-	print('The area is: ', total)
+	total = (3.14)*(diameter_passed_in/2)**2
+	print('The area is: ', total, 'cm^2')
 
 	return total;
 
-diameter = input('Put the diameter of the circle here: ')
-area = calculate_area_of_a_circle(diameter)
-print ("Area of circle is: " + area)
+def calculate_circumference(diameter_passed_in):
+	final = 2*(3.14)*(diameter_passed_in/2)
+	print('The circumference is: ', final, 'cm')
+
+	return final;
+
+diameter = input('Put the diameter in centimeters of the circle here: ')
+diameter_as_number = float(diameter)
+area = calculate_area_of_a_circle(diameter_as_number)
+circumference = calculate_circumference(diameter_as_number)
 
 input()
